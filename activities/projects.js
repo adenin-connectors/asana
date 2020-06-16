5702a232e86948b24d0ba553878b751f6bd523c1
olliejm <olliejm@protonmail.com>
2020-06-16 16:36:28 +0100

    update

--- activities/projects.js
+++ activities/projects.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const api = require('./common/api');
 
 module.exports = async function (activity) {
@@ -6,30 +7,32 @@ module.exports = async function (activity) {
     api.initialize(activity);
     const response = await api('/projects');
 
-    if ($.isErrorResponse(activity,response)) return;
+    if ($.isErrorResponse(activity, response)) return;
 
     activity.Response.Data = convertProjects(response);
   } catch (error) {
-    $.handleError(activity,error);
+    $.handleError(activity, error);
   }
 };
 
 //**maps response data*/
 function convertProjects(response) {
-  let items = [];
-  let data = response.body.data;
+  const items = [];
+  const data = response.body.data;
 
   for (let i = 0; i < data.length; i++) {
-    let raw = data[i];
-    let item = {
+    const raw = data[i];
+
+    items.push({
       id: raw.id,
       title: raw.name,
       description: raw.name,
       link: `https://app.asana.com/0/${raw.id}`,
       raw: raw
-    };
-    items.push(item);
+    });
   }
 
-  return { items: items };
+  return {
+    items: items
+  };
 }
